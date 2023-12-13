2b2a7464bced076a1333f5d170a6b838f3058eb0
BenFischer001 <benfischer52@gmail.com>
2023-12-12 19:17:47 -0600

    fix: when vid was displalyed framing was off - fixed by setting width + aspect ration of individual element

--- src/components/Display.js
+++ src/components/Display.js
@@ -4,14 +4,14 @@ import './Display.css';
 
 const Display = ({imgUrl, media_type}) => {
     return (
-        <>
+        < >
         {
         media_type == "image" ? ( <img src={imgUrl} className="item"></img> ) : (
             <iframe
-            className="item"
+            className="iframe"
             src={imgUrl}
             title="YouTube video player"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope"
             allowFullScreen
           ></iframe> 
         )
